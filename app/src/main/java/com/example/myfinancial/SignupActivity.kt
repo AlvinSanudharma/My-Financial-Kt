18573ea5db8fe191834884eed22a6c868b127c6b
AlvinSanudharma <alvinsanudharma@gmail.com>
2021-09-11 20:51:45 +0800

    Commit Message...

--- app/src/main/java/com/example/myfinancial/SignupActivity.kt
+++ app/src/main/java/com/example/myfinancial/SignupActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 
 class SignupActivity : AppCompatActivity() {
 
+//    testing push on github
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
